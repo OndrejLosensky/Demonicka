4d820dd8f75d9008056014088c518ef56cba4c6c
Ondřej Losenský <ondrej@losensky.dev>
2025-07-23 18:51:23 +0200

    swift app - prototype

---
+++ mobile/mobile/mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobileApp.swift
+//  mobile
+//
+//  Created by Ondřej Losenský on 23.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
