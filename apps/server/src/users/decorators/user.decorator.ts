5bbec0b33777f74c64d76ddd07b58e71008721d5
Ondřej Losenský <losenskyondrej@gmail.com>
2026-01-13 06:36:09 +0100

    finish migration to Prisma

--- apps/server/src/users/decorators/user.decorator.ts
+++ apps/server/src/users/decorators/user.decorator.ts
@@ -1,5 +1,5 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User } from '../entities/user.entity';
+import { User } from '@prisma/client';
 import { Request } from 'express';
 
 export const RequestUser = createParamDecorator(
