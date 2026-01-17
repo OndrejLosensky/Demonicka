eb6e5da1bf5ab95ee715e6f7007056b350a3d507
Ondřej Losenský <losenskyondrej@gmail.com>
2026-01-17 18:43:29 +0100

    fix participant creation

--- apps/server/src/auth/decorators/current-user.decorator.ts
+++ apps/server/src/auth/decorators/current-user.decorator.ts
@@ -2,12 +2,15 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from '@prisma/client';
 
 interface RequestWithUser extends Request {
-  user: User;
+  user?: User;
 }
 
 export const CurrentUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext): Omit<User, 'password'> => {
+  (data: unknown, ctx: ExecutionContext): Omit<User, 'password'> | undefined => {
     const request = ctx.switchToHttp().getRequest<RequestWithUser>();
+    if (!request.user) {
+      return undefined;
+    }
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password: omitted, ...userWithoutPassword } = request.user;
     return userWithoutPassword;
